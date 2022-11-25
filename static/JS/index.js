2c2b5206bc84dfd5895e4262955a34547da16c23
Andrius <114742531+Cavazzano1003@users.noreply.github.com>
2022-11-25 21:17:10 +0100

    aggiunto parte javascript

---
+++ static/JS/index.js
@@ -0,0 +1,23 @@
+function rosso(bottone){
+    document.body.style.backgroundColor="red";
+}
+
+function verdechiaro(bottone){
+    document.body.style.backgroundColor="green";
+}
+
+function blu(bottone){
+    document.body.style.backgroundColor="blue";
+}
+
+function verdescuro(bottone){
+    document.body.style.backgroundColor="pink";
+}
+
+function magenta(bottone){
+    document.body.style.backgroundColor="magenta";
+}
+
+function giallo(bottone){
+    document.body.style.backgroundColor="yellow";
+}
